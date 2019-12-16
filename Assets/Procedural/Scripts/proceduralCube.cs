e1598645a01366f4b6c6508ac6106dd92fc32c28
Joseph Jolton <j@jolton.com>
2019-12-16 13:21:22 -0600

    Added color variants

--- Assets/Procedural/Scripts/proceduralCube.cs
+++ Assets/Procedural/Scripts/proceduralCube.cs
@@ -7,22 +7,28 @@ public class proceduralCube : MonoBehaviour
     private float xSize;
     private float ySize;
     private float zSize;
-    public Color[] colorPalette;
+    public Color32[] colorPalette;
+    private Color32 selectedColor;
 
-    void Start()
-    {
-        xSize = Random.Range(1, 20);
-        ySize = Random.Range(1, 20);
-        zSize = Random.Range(1, 20);
-
-        transform.localScale = new Vector3(xSize, ySize, zSize);
-    }
+    //void Start()
+    //{
+    //    xSize = Random.Range(1, 10);
+    //    ySize = Random.Range(1, 10);
+    //    zSize = Random.Range(1, 10);
+    //    selectedColor = colorPalette[Random.Range(0, colorPalette.Length)];
+    //    Material cubeRenderer = GetComponent<Renderer>().material;
+    //    cubeRenderer.color = selectedColor;
+    //    transform.localScale = new Vector3(xSize, ySize, zSize);
+    //}
 
     public void Reset()
     {
-        xSize = Random.Range(1, 20);
-        ySize = Random.Range(1, 20);
-        zSize = Random.Range(1, 20);
+        xSize = Random.Range(.1f, 5);
+        ySize = Random.Range(.1f, 5);
+        zSize = Random.Range(.1f, 5);
+        selectedColor = colorPalette[Random.Range(0, colorPalette.Length)];
+        Material cubeRenderer = GetComponent<Renderer>().material;
+        cubeRenderer.color = selectedColor;
         transform.localScale = new Vector3(xSize, ySize, zSize);
     }
 }
