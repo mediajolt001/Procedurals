d28b209754dc23d4ac42ecf87aa0bbd0942df148
mediajolt001 <j@jolton.com>
2019-12-15 19:13:43 -0600

    Added procedural generation

---
+++ Assets/Procedural/Scripts/proceduralCube.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class proceduralCube : MonoBehaviour
+{
+    private float xSize;
+    private float ySize;
+    private float zSize;
+    public Color[] colorPalette;
+
+    void Start()
+    {
+        xSize = Random.Range(1, 20);
+        ySize = Random.Range(1, 20);
+        zSize = Random.Range(1, 20);
+
+        transform.localScale = new Vector3(xSize, ySize, zSize);
+    }
+
+    public void Reset()
+    {
+        xSize = Random.Range(1, 20);
+        ySize = Random.Range(1, 20);
+        zSize = Random.Range(1, 20);
+        transform.localScale = new Vector3(xSize, ySize, zSize);
+    }
+}
