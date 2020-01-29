e3fb6efc04a39a287163f4fdf698a556722c4964
Unknown <j@jolton.com>
2020-01-28 20:39:14 -0600

    Changed to newer version

--- Assets/Procedural/Scripts/proceduralCubeSizer.cs
+++ Assets/Procedural/Scripts/proceduralCubeSizer.cs
@@ -2,24 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class proceduralCube : MonoBehaviour
+public class proceduralCubeSizer : MonoBehaviour
 {
     private float xSize;
     private float ySize;
     private float zSize;
     [SerializeField] float sizeMin = .1f;
     [SerializeField] float sizeMax = 5f;
-    public Color32[] colorPalette;
-    private Color32 selectedColor;
 
-    public void Reset()
+    public void OnEnable()
     {
         xSize = Random.Range(sizeMin, sizeMax);
         ySize = Random.Range(sizeMin, sizeMax);
         zSize = Random.Range(sizeMin, sizeMax);
-        selectedColor = colorPalette[Random.Range(0, colorPalette.Length)];
-        Material cubeRenderer = GetComponent<Renderer>().material;
-        cubeRenderer.color = selectedColor;
         transform.localScale = new Vector3(xSize, ySize, zSize);
     }
 }
